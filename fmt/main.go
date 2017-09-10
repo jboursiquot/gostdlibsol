a21605398b75221c455b31823ed6e4ae19752866
Johnny Boursiquot <jboursiquot@gmail.com>
2017-09-10 18:25:02 -0400

    Print custom types using the Stringer interface

--- fmt/main.go
+++ fmt/main.go
@@ -3,27 +3,33 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"os"
 )
+
+type point struct{ x, y int64 }
+
+func (p point) String() string {
+	return fmt.Sprintf("Point(%d, %d)", p.x, p.y)
+}
 
 var (
 	i = 100
 	f = 3.14
 	b = true
 	s = "Clear is better than clever."
-	p = struct{ x, y int64 }{252, 101}
+	p = point{252, 101}
 )
 
 var bi int64 = -922337203685
 var ui uint64 = 1844679551615
 
 func main() {
-	n, err := outputToWriter()
-	if err != nil {
-		fmt.Printf("Error occured: %s\n", err)
-		os.Exit(-1)
-	}
-	fmt.Printf("Filename: %s\n", n)
+	outputToStdout()
+	// n, err := outputToWriter()
+	// if err != nil {
+	// 	fmt.Printf("Error occured: %s\n", err)
+	// 	os.Exit(-1)
+	// }
+	// fmt.Printf("Filename: %s\n", n)
 }
 
 func outputToWriter() (string, error) {
@@ -39,8 +45,10 @@ func outputToWriter() (string, error) {
 }
 
 func outputToStdout() {
-	fmt.Println(i, f, b, s, bi, ui, p)
-	fmt.Printf("i = %v\nf = %v\nb = %v\ns = %v\nbi = %v\nui = %v\np = %v\n", i, f, b, s, bi, ui, p)
-	fmt.Printf("i = %#v\nf = %#v\nb = %#v\ns = %#v\nbi = %#v\nui = %#v\np = %#v\n", i, f, b, s, bi, ui, p)
-	fmt.Printf("i = %T\nf = %T\nb = %T\ns = %T\nbi = %T\nui = %T\np = %T\n", i, f, b, s, bi, ui, p)
+	fmt.Println(p)
+	fmt.Printf("%v\n", p)
+	// fmt.Println(i, f, b, s, bi, ui, p)
+	// fmt.Printf("i = %v\nf = %v\nb = %v\ns = %v\nbi = %v\nui = %v\np = %v\n", i, f, b, s, bi, ui, p)
+	// fmt.Printf("i = %#v\nf = %#v\nb = %#v\ns = %#v\nbi = %#v\nui = %#v\np = %#v\n", i, f, b, s, bi, ui, p)
+	// fmt.Printf("i = %T\nf = %T\nb = %T\ns = %T\nbi = %T\nui = %T\np = %T\n", i, f, b, s, bi, ui, p)
 }
